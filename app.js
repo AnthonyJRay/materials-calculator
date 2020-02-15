d4c552acb3be2c3418307441aedc5c93eb138d24
Anthony Eriksen <ajrayid@gmail.com>
2020-02-15 11:16:31 -0700

    Added a check to input fields if empty.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+// Global Variables
 const addAreaBtn = document.querySelector('.add-area');
 const measurementsBody = document.querySelector('.measurements');
 const widths = document.querySelectorAll('.width-measurement');
 const calcBtn = document.querySelector('.calc-btn');
+const areaDisplay = document.querySelector('.display-area');
 
-// Add new area
+// Global Elements
+const span = document.createElement('span');
+
+areaDisplay.innerHTML = '0';
+
+// Add New Area button
 addAreaBtn.onclick = () => {
   let newArea = document.createElement('div');
   newArea.classList.add('newArea');
@@ -36,33 +43,52 @@ addAreaBtn.onclick = () => {
   measurementsBody.appendChild(newArea);
 };
 
+// Calculate button
 calcBtn.onclick = () => {
   const lengths = document.querySelectorAll('input.length-measurement');
   let lengthArr = [];
+  let lengthEmpty;
   lengths.forEach(length => {
-    const lengthStr = length.value;
-    const lengthInt = parseInt(lengthStr, 10);
-    lengthArr.push(lengthInt);
+    let lengthStr = length.value;
+    if (!lengthStr) {
+      lengthEmpty = true;
+    } else {
+      const lengthInt = parseInt(lengthStr, 10);
+      lengthArr.push(lengthInt);
+    }
   });
 
   let widths = document.querySelectorAll('input.width-measurement');
   let widthArr = [];
+  let widthEmpty;
   widths.forEach(width => {
     const widthStr = width.value;
+    if (!widthStr) {
+      widthEmpty = true;
+    }
     const widthInt = parseInt(widthStr, 10);
     widthArr.push(widthInt);
   });
 
-  let lengthSum = 0;
+  let totalSum = 0;
+  let areaSum;
   for (let i = 0; i < lengthArr.length; i++) {
-    lengthSum += lengthArr[i];
+    if (lengthArr.length === widthArr.length) {
+      areaSum = lengthArr[i] * widthArr[i];
+      totalSum += areaSum;
+      console.log(totalSum);
+    }
   }
 
-  let widthSum = 0;
-  for (let i = 0; i < widthArr.length; i++) {
-    widthSum += widthArr[i];
+  // Display total area in card
+  if (lengthEmpty || widthEmpty === true) {
+    areaDisplay.innerHTML = `
+    <p class="error">Missing Value!</p>
+    <p class="error">Please enter a value.</p>
+    `;
+  } else {
+    areaDisplay.innerHTML = totalSum;
   }
-
-  const totalSum = lengthSum * widthSum;
-  console.log(totalSum);
 };
+
+// Display total Area sqft
