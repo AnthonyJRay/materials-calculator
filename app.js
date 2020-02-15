29b0cf96c451ebf89ccb601f4b96294270c3e0d1
Anthony Eriksen <ajrayid@gmail.com>
2020-02-15 10:01:20 -0700

    Added dynamic Area Inputs, added the math to calculate total area.

--- app.js
+++ app.js
@@ -1,27 +1,68 @@
 const addAreaBtn = document.querySelector('.add-area');
 const measurementsBody = document.querySelector('.measurements');
+const widths = document.querySelectorAll('.width-measurement');
+const calcBtn = document.querySelector('.calc-btn');
 
+// Add new area
 addAreaBtn.onclick = () => {
   let newArea = document.createElement('div');
   newArea.classList.add('newArea');
 
+  let lengthWrapper = document.createElement('div');
+  lengthWrapper.classList.add('lengthWrapper');
   let lengthLabel = document.createElement('p');
   lengthLabel.classList.add('innerLabel');
   lengthLabel.innerHTML = 'Length';
   let areaLength = document.createElement('input');
   areaLength.type = 'text';
-  areaLength.classList.add('measurement');
+  areaLength.classList.add('length-measurement');
 
+  let widthWrapper = document.createElement('div');
+  widthWrapper.classList.add('widthWrapper');
   let widthLabel = document.createElement('p');
   widthLabel.classList.add('innerLabel');
   widthLabel.innerHTML = 'Width';
   let areaWidth = document.createElement('input');
   areaWidth.type = 'text';
-  areaWidth.classList.add('measurement');
+  areaWidth.classList.add('width-measurement');
 
-  newArea.appendChild(lengthLabel);
-  newArea.appendChild(areaLength);
-  newArea.appendChild(widthLabel);
-  newArea.appendChild(areaWidth);
+  lengthWrapper.appendChild(lengthLabel);
+  lengthWrapper.appendChild(areaLength);
+  widthWrapper.appendChild(widthLabel);
+  widthWrapper.appendChild(areaWidth);
+
+  newArea.appendChild(lengthWrapper);
+  newArea.appendChild(widthWrapper);
   measurementsBody.appendChild(newArea);
+};
+
+calcBtn.onclick = () => {
+  const lengths = document.querySelectorAll('input.length-measurement');
+  let lengthArr = [];
+  lengths.forEach(length => {
+    const lengthStr = length.value;
+    const lengthInt = parseInt(lengthStr, 10);
+    lengthArr.push(lengthInt);
+  });
+
+  let widths = document.querySelectorAll('input.width-measurement');
+  let widthArr = [];
+  widths.forEach(width => {
+    const widthStr = width.value;
+    const widthInt = parseInt(widthStr, 10);
+    widthArr.push(widthInt);
+  });
+
+  let lengthSum = 0;
+  for (let i = 0; i < lengthArr.length; i++) {
+    lengthSum += lengthArr[i];
+  }
+
+  let widthSum = 0;
+  for (let i = 0; i < widthArr.length; i++) {
+    widthSum += widthArr[i];
+  }
+
+  const totalSum = lengthSum * widthSum;
+  console.log(totalSum);
 };
