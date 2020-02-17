400ab06ff95eeda4a0d1579ae40f7bd4e7cd0fb4
Anthony Eriksen <ajrayid@gmail.com>
2020-02-17 14:18:28 -0700

    Added remove buttons to Rooms.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const dropdown = document.querySelector('.material-dropdown');
 const materialType = document.querySelector('.material-type-value');
 const materialAmount = document.querySelector('.material-amount-value');
 const boxSize = document.querySelector('.box-sizing-active');
+const roomsWrapper = document.querySelector('.rooms-wrapper');
 let materialSelected;
 let comesBoxed;
 let boxSizeNum;
@@ -31,16 +32,13 @@ dropdown.onchange = () => {
     boxSize.style.visibility = 'initial';
   }
 };
-const hello = document.getElementById('size-value-16').checked;
+
 if (document.querySelector('#size-value-16').checked) {
   boxSizeNum = 16;
-  console.log('16 is checked');
 } else if (document.querySelector('#size-value-20').checked) {
   boxSizeNum = 20;
-  console.log('20 is checked');
 } else if (document.querySelector('#size-value-24').checked) {
   boxSizeNum = 24;
-  console.log('24 is checked');
 }
 
 const materials = {
@@ -78,40 +76,45 @@ const materials = {
 
 // Add New Area button
 addAreaBtn.onclick = () => {
-  let newArea = document.createElement('div');
-  newArea.classList.add('newArea');
+  let delButton = document.createElement('button');
+  delButton.classList.add('delButton');
+  delButton.innerText = 'X';
 
-  let lengthWrapper = document.createElement('div');
-  lengthWrapper.classList.add('lengthWrapper');
-  let lengthLabel = document.createElement('p');
-  lengthLabel.classList.add('innerLabel');
+  let newRoom = document.createElement('li');
+  newRoom.classList.add('new-area');
+  let roomLength = document.createElement('input');
+  roomLength.type = 'text';
+  roomLength.classList.add('length-input');
+  let lengthLabel = document.createElement('span');
   lengthLabel.innerHTML = 'Length';
-  let areaLength = document.createElement('input');
-  areaLength.type = 'text';
-  areaLength.classList.add('length-measurement');
-
-  let widthWrapper = document.createElement('div');
-  widthWrapper.classList.add('widthWrapper');
-  let widthLabel = document.createElement('p');
-  widthLabel.classList.add('innerLabel');
-  widthLabel.innerHTML = 'Width';
-  let areaWidth = document.createElement('input');
-  areaWidth.type = 'text';
-  areaWidth.classList.add('width-measurement');
-
+  let lengthWrapper = document.createElement('div');
+  lengthWrapper.classList.add('length-measurement');
+  lengthWrapper.appendChild(roomLength);
   lengthWrapper.appendChild(lengthLabel);
-  lengthWrapper.appendChild(areaLength);
-  widthWrapper.appendChild(widthLabel);
-  widthWrapper.appendChild(areaWidth);
 
-  newArea.appendChild(lengthWrapper);
-  newArea.appendChild(widthWrapper);
-  measurementsBody.appendChild(newArea);
+  let roomWidth = document.createElement('input');
+  roomWidth.type = 'text';
+  roomWidth.classList.add('width-input');
+  let widthLabel = document.createElement('span');
+  widthLabel.innerHTML = 'Width';
+  let widthWrapper = document.createElement('div');
+  widthWrapper.classList.add('width-measurement');
+  widthWrapper.appendChild(roomWidth);
+  widthWrapper.appendChild(widthLabel);
+
+  newRoom.appendChild(delButton);
+  newRoom.appendChild(lengthWrapper);
+  newRoom.appendChild(widthWrapper);
+  roomsWrapper.appendChild(newRoom);
+
+  delButton.addEventListener('click', function() {
+    delButton.parentElement.remove();
+  });
 };
 
 // Calculate button
 calcBtn.onclick = () => {
-  const lengths = document.querySelectorAll('input.length-measurement');
+  const lengths = document.querySelectorAll('input.length-input');
   let lengthArr = [];
   let lengthEmpty;
   lengths.forEach(length => {
@@ -124,7 +127,7 @@ calcBtn.onclick = () => {
     }
   });
 
-  let widths = document.querySelectorAll('input.width-measurement');
+  let widths = document.querySelectorAll('input.width-input');
   let widthArr = [];
   let widthEmpty;
   widths.forEach(width => {
@@ -167,6 +170,12 @@ calcBtn.onclick = () => {
     if (property === materialSelected) {
       materialType.innerHTML = materials[property].type;
       materialAmount.innerHTML = materials[property].calc(totalSum).toFixed(1);
+    }
+  }
+
+  if (materialSelected !== 'carpet' && materialSelected !== 'linoleum') {
+    if (boxSizeNum === undefined || boxSizeNum === '') {
+      materialAmount.innerHTML = 'Please Select a Box Size';
     }
   }
 };
